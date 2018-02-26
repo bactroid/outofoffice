a309fe0c94cf85599a9ff85c148fc528ddf3edd8
Kate Haskell <khaskell@ufl.edu>
2018-02-26 10:04:27 -0500

    Added Haskell-style type strings as comments in message.js

--- src/message.js
+++ src/message.js
@@ -3,9 +3,11 @@ const fs = require('fs')
 
 const readFileAsync = promisify(fs.readFile)
 
+// getMessageJSON :: null -> Promise JSON
 const getMessageJSON = () =>
       readFileAsync('../message.json', 'utf8').then(JSON.parse)
 
+// getMessageText :: null -> Promise String
 const getMessageText = () => getMessageJSON().then(x => x.message)
 
 module.exports = {
