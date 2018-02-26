df9f3d11ed0ca30ed7aadfd335dd77a27fe0f9ca
Kate Haskell <khaskell@ufl.edu>
2018-02-26 10:00:32 -0500

    Added getMessageText to return just the text from message.json

--- src/message.js
+++ src/message.js
@@ -6,6 +6,9 @@ const readFileAsync = promisify(fs.readFile)
 const getMessageJSON = () =>
       readFileAsync('../message.json', 'utf8').then(JSON.parse)
 
+const getMessageText = () => getMessageJSON().then(x => x.message)
+
 module.exports = {
-  getMessageJSON
+  getMessageJSON,
+  getMessageText
 }
--- src/message.test.js
+++ src/message.test.js
@@ -15,3 +15,16 @@ describe('getMessageJSON', () => {
       })
   })
 })
+
+describe('getMessageText', () => {
+  test('Returns text from message.json', () => {
+    const expected = 'I will be out of the office from STARTDT through ENDDT.\n\nFor immediate assistance, please reach out the UF Computing Help Desk at 352-392-4357. Their service desk will create a support ticket and will get in touch with UFIT personnel who can assist you in my absence.'
+    message.getMessageText()
+      .then(resp => {
+        expect(resp).toBe(expected)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  })
+})
